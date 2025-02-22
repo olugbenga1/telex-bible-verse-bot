51114cb5c2e75c4a4c24c7e270d8343e3268812e
K Odedele <odedeleg@gmail.com>
2025-02-22 10:17:51 -0600

    chore: add more time options to configuration options and allowed cors from all origin

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'https://tv43bl8t-4000.use.devtunnels.ms/',
-      'https://telex.im',
-      'https://telex-bible-verse-bot.onrender.com/',
-    ],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
